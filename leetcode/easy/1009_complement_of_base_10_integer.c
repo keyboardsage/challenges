17bd6e6554ce70a75986ea393d348329a9f573ac
keyboardsage <keyboardsage@users.noreply.github.com>
2023-09-30 20:25:20 -0400

    Added trivial case to #1009

--- leetcode/easy/1009_complement_of_base_10_integer.c
+++ leetcode/easy/1009_complement_of_base_10_integer.c
@@ -1,6 +1,8 @@
 int bitwiseComplement(int n){
     const int RADIX = 2;
 
+    if (n == 0) return 1; // trivial case
+    
     // build a mask where ones are in the indices necessary to represent `n` in binary
     unsigned int mask = 0;
     for (int i = 0; pow(RADIX, i) < n; i++) mask |= 1 << i;
